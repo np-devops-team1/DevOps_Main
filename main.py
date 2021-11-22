da931ff39d7498394406fad91d17ac32b19e268f
iamgruuten <52743433+iamgruuten@users.noreply.github.com>
2021-11-22 09:24:01 +0800

    clear main.py

--- main.py
+++ main.py
@@ -24,31 +24,3 @@
 
 
 #Imports
-import random
-
-#Main Menu
-#----------------------#
-
-class Game():
-    
-    def displayMainMenu():
-        #optionsMain = ['1. Start new game','2. Load saved game','0. Exit']
-        print('Welcome, mayor of Simp City!')
-        print('----------------------------')
-        print('1. Start new game')
-        print('2. Load saved game')
-        print('0. Exit')
-        print('Your choice?')
-
-    def displayGameMenu(random1,random2):
-        if(random1 in range(1, 6)) and (random2 in range(1, 6)):
-            buildings = ['BCH' , 'FAC' , 'HSE' , 'SHP' , 'HWY']
-            print(f'1. Build a {buildings[random1]}')
-            print(f'2. Build a {buildings[random2]}')
-            print(f'3. See remaining buildings')
-            print(f'4. See current score')
-            print()
-            print(f'5. Save game')
-            print(f'0. Exit to main menu')
-        else:
-            raise ValueError("Value Error")
