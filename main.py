1e837f0aaf4ea0f630f478e6dd0031135071ed80
iamgruuten <52743433+iamgruuten@users.noreply.github.com>
2021-11-23 17:46:19 +0800

    style added missing white-spaces

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#The Simp Game
+# The Simp Game
 
 # Buildings
-#----------------------#
+# ---------------------- #
 # Beach (BCH) , Factory (FAC) , House (HSE) , Shop (SHP) , Highway (HWY)
 
-#Board
-#----------------------#
+# Board
+# ---------------------- #
 # [['A1','B1','C1','D1'],
 #  ['A2','B2','C2','D2'],
 #  ['A3','B3','C3','D3'],
 #  ['A4','B4','C4','D4']]
 
-#Game Options
-#-----------------------#
+# Game Options
+# -----------------------#
 # 6 Options
 # 1. Place Random Building
 # 2. Place Random Building
@@ -22,8 +22,7 @@
 # 5. Save game
 # 0. Exit to main menu
 
-#Imports
-import random
+# Imports
 
 # function for main menu
 def MenuSelection(option):
@@ -37,6 +36,7 @@ def MenuSelection(option):
         print("invalid option, try again")
         FirstMenu()
 
+
 def FirstMenu():
     print("Welcome, mayor of Simp City!")
     print("------------------------------")
@@ -46,8 +46,10 @@ def FirstMenu():
     option = (int)(input("Your choice?"))
     MenuSelection(option)
 
+
 def StartGameFunc():
     print("start")
+
 
 def LoadGameFunc():
     print("load")
